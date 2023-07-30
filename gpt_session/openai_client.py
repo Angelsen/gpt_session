9ce9eef854365244fdd398a46ec2d218f64bf099
Fredrik Angelsen <fredrikangelsen@gmail.com>
2023-07-30 20:26:35 +0200

    Clean up ignored files

---
+++ gpt_session/openai_client.py
@@ -0,0 +1,24 @@
+import openai
+
+openai.api_key = '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'
+
+class OpenAIClient:
+    def ask(self, question, last_command=None, last_output=None, conversation=None):
+        if last_command and last_output:
+            system_message = f"You are a helpful assistant with expertise in terminal commands. The user just ran the command `{last_command}` and received the following output: `{last_output}`. Please consider this when answering the following question."
+        else:
+            system_message = "You are a helpful assistant that provides guidance, examples, and explanations for terminal commands. The user has a question."
+        
+        if conversation:
+            conversation.append({"role": "user", "content": question})
+        else:
+            conversation = [{"role": "system", "content": system_message}, {"role": "user", "content": question}]
+        
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=conversation
+        )
+        
+        conversation.append({"role": "assistant", "content": response['choices'][0]['message']['content']})
+        
+        return response['choices'][0]['message']['content']
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="GPT-Session",
+    version="0.1",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "guide-session = gpt_session.terminal_handler:main",
+        ],
+    },
+
+    install_requires=[
+        "openai",
+    ],
+)
